17dd79ac825922053d525cd67d03113af38b3580
Pixelfy <info@pixelfy.nl>
2015-10-06 14:35:35 +0200

    Switched to example db | added example 404 view

---
+++ app/views/layouts/404.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 oooohhh noooes!</title>
+</head>
+<body>
+<h2 style="text-align: center;">Sterf! X.X</h2>
+</body>
+</html>
