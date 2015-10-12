b0a88fddd1b17f6ef07bf029fa680a35abeaa4d7
Pixelfy <info@pixelfy.nl>
2015-10-12 15:50:55 +0200

    Center each two members, added images

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,15 +1,15 @@
 <section class="about">
 
 	<div class="container">
-		<h2 class="">The people behind PrimeTwo</h2>
-		<div class="row">
-			<?php foreach ($team as $member): ?>
-				<div class="col-lg-4 ta-center">
-					<h3><?php echo $member->fullname; ?></h3>
-					<img class="team-member-picture img-circle" src="<?php echo $appConfig['url'].$member->picture; ?>" alt="<?php echo $member->fullName; ?>'s picture.">
-				</div>
-			<?php endforeach; ?>
-		</div>
-	</div>
+		<h2 class="text-center">The people behind PrimeTwo</h2>
+            <div class="row">
+                <?php foreach ($team as $member): ?>
+                    <div class="col-lg-6 ta-center">
+                        <h3><?php echo $member->fullname; ?></h3>
+                        <img class="team-member-picture img-circle" src="<?php echo $appConfig['url'].$member->picture; ?>" alt="<?php echo $member->fullName; ?>'s picture.">
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
 	
 </section>
