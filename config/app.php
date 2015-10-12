0ef51ba09fb96f5556e24e1c41eea22e09bec064
Koen Hendriks <info@pixelfy.nl>
2015-10-12 19:43:18 +0200

    Explained facades

--- config/app.php
+++ config/app.php
@@ -17,6 +17,6 @@ return array(
     // Facades for including classes with static functions ( like render )
     'Facades' => array(
         'View' => 'PrimeTwo\Resources\View',
-        //'FormValidation' => 'PrimeTwo\Framework\FormValidation' TODO: hoe werkt dit eigenlijk @Koen ?
+        //'FormValidation' => 'PrimeTwo\Framework\FormValidation' TODO: hoe werkt dit eigenlijk @Koen ? @Peter: http://static.comicvine.com/uploads/original/11112/111128444/3656917-magic-spongebob-300x225.jpg
     )
 );
